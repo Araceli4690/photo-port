a57674b2fb832a2c7bdeaf57fb5ffdfde84aec2f
araceli <carmona.araceli.4690@gmail.com>
2021-08-24 19:42:34 -0700

    photolist and gallery components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-//import react into all component files
-import React from 'react';
-//you can import any css or image at top
-
-import About from './components/About';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
-
-
+import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div >
-      <Nav></Nav>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
-
     </div>
   );
 }
